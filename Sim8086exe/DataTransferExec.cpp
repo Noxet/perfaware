8380b9acff57a80d46857f0e4b27980f84270476
Jonathan Sönnerup <nooxet@gmail.com>
2023-04-10 00:06:04 +0200

    solved listing 43 and 44

--- Sim8086exe/DataTransferExec.cpp
+++ Sim8086exe/DataTransferExec.cpp
@@ -1,6 +1,8 @@
 #include "DataTransferExec.h"
 #include "Cpu.h"
 
+#define REGIDX(x) (x.Register.Index - 1)
+
 
 u8 dataTransferExec(const instruction *instr)
 {
@@ -8,13 +10,9 @@ u8 dataTransferExec(const instruction *instr)
 	{
 	case Op_mov:
 		{
-			cout << printf("%s ", Sim86_MnemonicFromOperationType(instr->Op)) << endl;
 			const instruction_operand dest = instr->Operands[0];
 			if (dest.Type == Operand_Register)
 			{
-				printf("idx: %d, offset: %d, count: %d\n", instr->Operands[0].Register.Index,
-				       instr->Operands[0].Register.Offset, instr->Operands[0].Register.Count);
-
 				s32 val{};
 				const instruction_operand src = instr->Operands[1];
 				if (src.Type == Operand_Immediate)
@@ -23,7 +21,7 @@ u8 dataTransferExec(const instruction *instr)
 				}
 				else if (src.Type == Operand_Register)
 				{
-					val = REGS[src.Register.Index];
+					val = REGS[REGIDX(src)];
 				}
 
 				if (dest.Register.Count == 1)
@@ -35,7 +33,7 @@ u8 dataTransferExec(const instruction *instr)
 					val = val & 0xffff;
 				}
 
-				REGS[dest.Register.Index - 1] = val;
+				REGS[REGIDX(dest)] = val;
 			}
 		}
 		break;
